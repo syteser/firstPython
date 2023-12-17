873b53bfcce055ad1c68c74bf417ab479d628d92
sergeysergey <savchenko.sergey.v@gmail.com>
2023-12-17 09:40:18 +0200

    first Commit

---
+++ game.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("123")
+    bg_color=(0,0,0)
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+run()
