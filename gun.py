6e9bf32e4f716605301ac115debcac44278048e6
sergeysergey <savchenko.sergey.v@gmail.com>
2023-12-17 12:09:43 +0200

    добавлены обработка кнопок вправо/влево

--- gun.py
+++ gun.py
@@ -1,5 +1,6 @@
 import pygame
 
+snap =5
 class Gun():
 
     def __init__(self, screen):
@@ -9,8 +10,14 @@ class Gun():
         self.screen_rect=screen.get_rect()
         self.rect.centerx=self.screen_rect.centerx
         self.rect.bottom=self.screen_rect.bottom
-
+        self.mright=False
+        self.mleft=False
 
     def output(self):
         self.screen.blit(self.image, self.rect)
 
+    def update_gun(self):
+        if self.mright and self.rect.right < self.screen_rect.right:
+            self.rect.centerx+=snap
+        if self.mleft and self.rect.left >0:
+            self.rect.centerx-=snap
