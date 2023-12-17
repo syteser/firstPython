dfd4c3c04ef4d46bfb650dbe82628a8cc71f78b7
sergeysergey <savchenko.sergey.v@gmail.com>
2023-12-17 10:25:25 +0200

    class Gun

--- gun.py
+++ gun.py
@@ -1,5 +1,15 @@
 import pygame
 
-def __init__():
-    pass
+class Gun():
 
+    def __init__(self, screen):
+        self.screen=screen
+        self.image=pygame.image.load('image/gun')
+        self.rect=self.image.get_rect()
+        self.screen_rect=self.get_rect()
+        self.rect.centerx=self.screen_rect.centerx
+        self.rect.buttom=self.screen_rect.buttom
+
+
+    def output(self):
+        pass
