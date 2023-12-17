a974da9d5eee83c095ed91b870ac299e52bed1fd
syteser <savchenko.sergey.v@gmail.com>
2023-12-17 11:01:25 +0200

    class gun + image gun

--- game.py
+++ game.py
@@ -4,21 +4,23 @@ from gun import Gun
 
 game_name = "Космические защитники"
 
+
 def run():
     """инициализация главного окна игры
     устанавливапем разрешение экрана, цвет фона, заголовок окна..."""
     pygame.init()
     screen = pygame.display.set_mode((1200, 650))
     pygame.display.set_caption(game_name)
-    bg_color=(0,0,0)
+    bg_color = (0, 0, 0)
     gun = Gun(screen)
 
     while True:
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
+            if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(bg_color)
         gun.output()
         pygame.display.flip()
+
 
 run()
--- gun.py
+++ gun.py
@@ -1,16 +1,15 @@
 import pygame
 
-class Gun():
+
+class Gun:
 
     def __init__(self, screen):
-        self.screen=screen
-        self.image=pygame.image.load('image/gun.png')
-        self.rect=self.image.get_rect()
-        self.screen_rect=screen.get_rect()
-        self.rect.centerx=self.screen_rect.centerx
-        self.rect.bottom=self.screen_rect.bottom
-
+        self.screen = screen
+        self.image = pygame.image.load('image/gun.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
     def output(self):
         self.screen.blit(self.image, self.rect)
-
